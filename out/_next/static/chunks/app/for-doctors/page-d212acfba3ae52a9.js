f9d260478bfe1c23c1afd1a9f89429a7ae111a75
Z User <z@container>
2026-06-22 08:14:50 +0000

    chore: gitignore build output (out/)

--- out/_next/static/chunks/app/for-doctors/page-d212acfba3ae52a9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6],{7707:function(e,n,s){Promise.resolve().then(s.bind(s,8716)),Promise.resolve().then(s.bind(s,2718)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[207,972,569,971,117,744],function(){return e(e.s=7707)}),_N_E=e.O()}]);
